3be73d81bea423b6b7aa989018b04666fb4b5e2b
Leonhard Riedisser <leo008180@googlemail.com>
2020-03-14 22:56:06 +0100

    Switch back to validation-based environment parsing
    Hoping that this will make the app work in k8s environment

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import           Config
 import           Control.Concurrent.Async
 import           Data.IORef
+import           Data.Validation
 import           Lib
 import           Server
 import           System.Environment
@@ -11,6 +12,9 @@ main :: IO ()
 main = do
   env <- getEnvironment
   _ <- putStrLn $ unlines $ show <$> env
-  config <- parseConfig
-  statuses <- newIORef []
-  concurrently_ (updateStatusesRegularly config statuses) (runServer config statuses)
+  configFromEnv <- parseConfigFromEnv
+  case configFromEnv of
+    Success config -> do
+      statuses <- newIORef []
+      concurrently_ (updateStatusesRegularly config statuses) (runServer config statuses)
+    Failure errors -> putStrLn $ showErrors errors
