984c7003338737b4fb703ea0705499904df723ce
Leonhard Riedisser <leo008180@googlemail.com>
2020-03-14 21:57:17 +0100

    Print whole environment before parsing

--- app/Main.hs
+++ app/Main.hs
@@ -5,9 +5,12 @@ import           Control.Concurrent.Async
 import           Data.IORef
 import           Lib
 import           Server
+import           System.Environment
 
 main :: IO ()
 main = do
+  env <- getEnvironment
+  _ <- putStrLn $ unlines $ show <$> env
   config <- parseConfig
   statuses <- newIORef []
   concurrently_ (updateStatusesRegularly config statuses) (runServer config statuses)
