fc48f75defc84163e146b3975a2ba6ba528b40fd
Leonhard Riedisser <leo008180@googlemail.com>
2020-03-15 19:11:52 +0100

    Replace Scotty by Servant

--- src/Server.hs
+++ src/Server.hs
@@ -1,24 +1,39 @@
+{-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeOperators     #-}
 
 module Server where
 
 import           Config
-import           Control.Monad.IO.Class        (liftIO)
+import           Control.Monad.IO.Class   (liftIO)
 import           Data.IORef
+import qualified Data.Text                as T
 import           Html
 import           Lib
 import           Metrics
-import           Text.Blaze.Html.Renderer.Text (renderHtml)
-import           TextShow                      (fromText, toLazyText)
-import           Web.Scotty                    hiding (status)
+import           Network.Wai.Handler.Warp
+import           Servant
+import           Servant.HTML.Blaze
+import qualified Text.Blaze.Html5         as H
+
+type Health = "health" :> Get '[ PlainText] T.Text
+
+type Metrics = "metrics" :> Get '[ PlainText] T.Text
+
+type Ui = "ui" :> Get '[ HTML] H.Html
+
+type API = Health :<|> Metrics :<|> Ui
+
+api :: Proxy API
+api = Proxy
 
 runServer :: Config -> IORef [Result] -> IO ()
-runServer config ioref =
-  scotty 8080 $ do
-    get "/" $ do
-      results <- liftIO $ readIORef ioref
-      html . renderHtml $ template (uiUpdateIntervalSecs config) results
-    get "/metrics" $ do
-      results <- liftIO $ readIORef ioref
-      text $ toLazyText . fromText $ createMetrics results
-    get "/health" $ text "UP"
+runServer config ioref = run 8080 . serve api $ return "health" :<|> liftIO mx :<|> liftIO htmlUi
+  where
+    mx :: IO T.Text
+    mx = do
+      results <- readIORef ioref
+      pure $ createMetrics results
+    htmlUi = do
+      results <- readIORef ioref
+      pure $ template (uiUpdateIntervalSecs config) results
