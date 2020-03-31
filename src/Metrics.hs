bea9ff51cfa3d4b47f7d6ce82dee55245b94f026
Leonhard Riedisser <leo008180@googlemail.com>
2020-03-31 18:45:25 +0200

    Add metric for successful builds, rename metric for failed builds

--- src/Metrics.hs
+++ src/Metrics.hs
@@ -4,7 +4,7 @@ module Metrics where
 
 import Control.Monad.IO.Class (MonadIO)
 import Data.IORef (IORef, readIORef)
-import qualified Data.Text as T
+import Data.List (foldl')
 import Katip
 import Lens.Micro
 import Lib hiding (name)
@@ -14,44 +14,63 @@ import qualified System.Metrics.Prometheus.Metric.Gauge as P
 import qualified System.Metrics.Prometheus.RegistryT as P
 import System.Metrics.Prometheus.Ridley
 import System.Metrics.Prometheus.Ridley.Types
-import TextShow (showt)
-
-createMetrics :: [Result] -> T.Text
-createMetrics rs = T.unlines (header <> content)
-  where
-    header = ["# HELP build_status_gauge committed offsets", "# TYPE build_status_gauge gauge"]
-    content = convertMetric <$> rs
-
-convertMetric :: Result -> T.Text
-convertMetric (Result name status _) = mconcat ["build_status_gauge{repository=\"", name, "\"} ", (showt . toMetricValue) status]
 
 initializeMetricsMiddleware :: IORef [Result] -> IO (Maybe WaiMetrics)
 initializeMetricsMiddleware ioref = do
   stdoutS <- mkHandleScribe ColorIfTerminal stdout (permitItem InfoS) V2
   let opts =
-        newOptions [("app", "gitlab-ci-build-statuses")] (brokenBuildsMetric ioref : defaultMetrics)
+        newOptions [("app", "gitlab-ci-build-statuses")] (defaultMetrics <> initializeBuildMetrics ioref)
           & prometheusOptions . samplingFrequency .~ 5
           & (dataRetentionPeriod ?~ 60)
           & katipScribes .~ ("GitlabCiBuildStatuses", [("stdout", stdoutS)])
   ctx <- startRidley opts ["metrics"] 8181
   pure $ ctx ^. ridleyWaiMetrics
 
-brokenBuildsMetric :: IORef [Result] -> RidleyMetric
-brokenBuildsMetric ioref = CustomMetric "build_statuses_broken_builds" (processBrokenBuilds ioref)
+initializeBuildMetrics :: IORef [Result] -> [RidleyMetric]
+initializeBuildMetrics results = (\m -> m results) <$> [failedBuildsMetric, successfulBuildsMetric]
 
-processBrokenBuilds :: MonadIO m => IORef [Result] -> RidleyOptions -> P.RegistryT m RidleyMetricHandler
-processBrokenBuilds ioref opts = do
+failedBuildsMetric :: IORef [Result] -> RidleyMetric
+failedBuildsMetric ioref = CustomMetric "build_statuses_failed_builds" (processFailedBuilds ioref)
+
+processFailedBuilds :: MonadIO m => IORef [Result] -> RidleyOptions -> P.RegistryT m RidleyMetricHandler
+processFailedBuilds ioref opts = do
   let popts = opts ^. prometheusOptions
-  openFD <- P.registerGauge "build_statuses_broken_builds" (popts ^. labels)
+  openFD <- P.registerGauge "build_statuses_failed_builds" (popts ^. labels)
   return
     RidleyMetricHandler
       { metric = openFD,
-        updateMetric = updateBrokenBuilds ioref,
+        updateMetric = updateFailedBuilds ioref,
         flush = False
       }
 
-updateBrokenBuilds :: IORef [Result] -> P.Gauge -> Bool -> IO ()
-updateBrokenBuilds ioref gauge _ = do
+updateFailedBuilds :: IORef [Result] -> P.Gauge -> Bool -> IO ()
+updateFailedBuilds ioref gauge _ = do
   results <- readIORef ioref
-  let numBroken = length $ filter (\res -> buildStatus res == Failed) results
-  P.set (fromIntegral numBroken) gauge
+  let numFailed = countResultsWithStatus Failed results
+  P.set (fromIntegral numFailed) gauge
+
+successfulBuildsMetric :: IORef [Result] -> RidleyMetric
+successfulBuildsMetric ioref = CustomMetric "build_statuses_successful_builds" (processSuccesfulBuilds ioref)
+
+processSuccesfulBuilds :: MonadIO m => IORef [Result] -> RidleyOptions -> P.RegistryT m RidleyMetricHandler
+processSuccesfulBuilds ioref opts = do
+  let popts = opts ^. prometheusOptions
+  openFD <- P.registerGauge "build_statuses_successful_builds" (popts ^. labels)
+  return
+    RidleyMetricHandler
+      { metric = openFD,
+        updateMetric = updateSuccesfulBuilds ioref,
+        flush = False
+      }
+
+updateSuccesfulBuilds :: IORef [Result] -> P.Gauge -> Bool -> IO ()
+updateSuccesfulBuilds ioref gauge _ = do
+  results <- readIORef ioref
+  let numSuccessful = countResultsWithStatus Successful results
+  P.set (fromIntegral numSuccessful) gauge
+
+countResultsWithStatus :: Foldable t => BuildStatus -> t Result -> Int
+countResultsWithStatus status = count (\res -> buildStatus res == status)
+
+count :: Foldable t => (a -> Bool) -> t a -> Int
+count f = foldl' (\acc a -> if f a then acc + 1 else acc) 0
