20c031ff8d8bf547f4254e68d1f7ab83540b2b44
Leonhard Riedisser <leo008180@googlemail.com>
2021-03-26 18:22:17 +0100

    Add OverallStatus for determining the combined status for a list of BuildStatuses

--- src/Core/Lib.hs
+++ src/Core/Lib.hs
@@ -34,6 +34,8 @@ module Core.Lib
     Project (..),
     Ref (..),
     isHealthy,
+    overallStatus,
+    OverallStatus (..),
   )
 where
 
@@ -263,3 +265,64 @@ isHealthy Skipped = False
 isHealthy Successful = True
 isHealthy SuccessfulWithWarnings = False
 isHealthy WaitingForResource = True
+
+data OverallStatus
+  = OverallSuccessful
+  | OverallSuccessfulRunning
+  | OverallFailedRunning
+  | OverallRunning
+  | OverallWarningRunning
+  | OverallFailed
+  | OverallWarning
+  | OverallUnknown
+  deriving (Bounded, Enum, Eq, Show)
+
+instance Semigroup OverallStatus where
+  OverallUnknown <> s = s
+  s <> OverallUnknown = s
+  OverallFailedRunning <> _ = OverallFailedRunning
+  _ <> OverallFailedRunning = OverallFailedRunning
+  OverallFailed <> OverallRunning = OverallFailedRunning
+  OverallRunning <> OverallFailed = OverallFailedRunning
+  OverallFailed <> OverallWarningRunning = OverallFailedRunning
+  OverallFailed <> OverallSuccessfulRunning = OverallFailedRunning
+  OverallFailed <> _ = OverallFailed
+  OverallWarningRunning <> OverallFailed = OverallFailedRunning
+  OverallSuccessfulRunning <> OverallFailed = OverallFailedRunning
+  _ <> OverallFailed = OverallFailed
+  OverallWarningRunning <> _ = OverallWarningRunning
+  _ <> OverallWarningRunning = OverallWarningRunning
+  OverallWarning <> OverallRunning = OverallWarningRunning
+  OverallRunning <> OverallWarning = OverallWarningRunning
+  OverallWarning <> OverallSuccessfulRunning = OverallWarningRunning
+  OverallSuccessfulRunning <> OverallWarning = OverallWarningRunning
+  OverallWarning <> _ = OverallWarning
+  _ <> OverallWarning = OverallWarning
+  OverallSuccessfulRunning <> _ = OverallSuccessfulRunning
+  _ <> OverallSuccessfulRunning = OverallSuccessfulRunning
+  OverallRunning <> OverallRunning = OverallRunning
+  OverallSuccessful <> OverallRunning = OverallSuccessfulRunning
+  OverallRunning <> OverallSuccessful = OverallSuccessfulRunning
+  OverallSuccessful <> _ = OverallSuccessful
+
+instance Monoid OverallStatus where
+  mempty = OverallUnknown
+
+overallStatus :: BuildStatuses -> OverallStatus
+overallStatus NoSuccessfulUpdateYet = OverallUnknown
+overallStatus (Statuses (_, statuses)) = foldMap (resultToOverall . buildStatus) statuses
+
+resultToOverall :: BuildStatus -> OverallStatus
+resultToOverall Unknown = OverallUnknown
+resultToOverall Cancelled = OverallFailed
+resultToOverall Created = OverallRunning
+resultToOverall Failed = OverallFailed
+resultToOverall Manual = OverallWarning
+resultToOverall Pending = OverallRunning
+resultToOverall Preparing = OverallRunning
+resultToOverall Running = OverallRunning
+resultToOverall Scheduled = OverallRunning
+resultToOverall Skipped = OverallWarning
+resultToOverall Successful = OverallSuccessful
+resultToOverall SuccessfulWithWarnings = OverallWarning
+resultToOverall WaitingForResource = OverallWarning
---
+++ test/OverallStatusSpec.hs
@@ -0,0 +1,87 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module OverallStatusSpec where
+
+import Core.Lib (OverallStatus (..))
+import Hedgehog
+import qualified Hedgehog.Gen as Gen
+import qualified Hedgehog.Range as Range
+import Relude
+import Test.Hspec
+import Test.Hspec.Hedgehog
+
+spec :: Spec
+spec = do
+  describe "OverallStatus" $ do
+    describe "monoid instance" $ do
+      it "adheres to left identity" $ hedgehog $ monoidLeftIdentity overallStatusGen
+      it "adheres to right identity" $ hedgehog $ monoidRightIdentity overallStatusGen
+      it "mappend behaves like (<>)" $ hedgehog $ monoidMappendCombine overallStatusGen
+    describe "folding" $ do
+      it "(<>) is commutative" $
+        hedgehog $ do
+          s1 <- forAll overallStatusGen
+          s2 <- forAll overallStatusGen
+          s1 <> s2 === s2 <> s1
+      it "folding respects the hierarchy of the statuses" $ hedgehog resultToOverallProps
+
+resultToOverallProps :: Monad m => PropertyT m ()
+resultToOverallProps = do
+  statuses <- forAll $ Gen.list (Range.linear 0 100) overallStatusGen
+  let result = fold statuses
+  case result of
+    OverallSuccessful -> assert $ all (== OverallSuccessful) statuses
+    OverallUnknown -> assert $ all (== OverallUnknown) statuses
+    OverallSuccessfulRunning ->
+      assert $
+        all (`elem` [OverallSuccessful, OverallSuccessfulRunning, OverallRunning, OverallUnknown]) statuses
+          && atLeastOneOf [OverallSuccessfulRunning, OverallRunning] statuses
+    OverallFailed ->
+      assert $
+        all (`elem` [OverallSuccessful, OverallFailed, OverallWarning, OverallUnknown]) statuses
+          && noneOf [OverallRunning, OverallSuccessfulRunning, OverallWarningRunning, OverallFailedRunning] statuses
+          && elem OverallFailed statuses
+    OverallFailedRunning ->
+      assert $
+        all (`elem` [OverallSuccessful, OverallSuccessfulRunning, OverallFailed, OverallFailedRunning, OverallWarning, OverallWarningRunning, OverallRunning, OverallUnknown]) statuses
+          && atLeastOneOf [OverallFailed, OverallFailedRunning] statuses
+          && atLeastOneOf [OverallSuccessfulRunning, OverallFailedRunning, OverallWarningRunning, OverallRunning] statuses
+    OverallWarning ->
+      assert $
+        all (`elem` [OverallSuccessful, OverallWarning, OverallUnknown]) statuses
+          && noneOf [OverallRunning, OverallSuccessfulRunning, OverallWarningRunning, OverallFailedRunning] statuses
+          && elem OverallWarning statuses
+    OverallWarningRunning ->
+      assert $
+        all (`elem` [OverallSuccessful, OverallSuccessfulRunning, OverallWarning, OverallWarningRunning, OverallRunning, OverallUnknown]) statuses
+          && atLeastOneOf [OverallWarning, OverallWarningRunning] statuses
+          && atLeastOneOf [OverallSuccessfulRunning, OverallWarningRunning, OverallRunning] statuses
+    OverallRunning ->
+      assert $
+        all (`elem` [OverallRunning, OverallUnknown]) statuses
+          && elem OverallRunning statuses
+
+atLeastOneOf :: (Functor t, Foldable t, Eq a) => t a -> [a] -> Bool
+atLeastOneOf mustIncludes as = or ((`elem` as) <$> mustIncludes)
+
+noneOf :: (Functor t, Foldable t, Eq a) => t a -> [a] -> Bool
+noneOf mustNotIncludes as = and ((`notElem` as) <$> mustNotIncludes)
+
+overallStatusGen :: Gen OverallStatus
+overallStatusGen = Gen.enumBounded
+
+monoidLeftIdentity :: (Monad m, Show a, Eq a, Monoid a) => Gen a -> PropertyT m ()
+monoidLeftIdentity gen = do
+  a <- forAll gen
+  mempty <> a === a
+
+monoidRightIdentity :: (Monad m, Show a, Eq a, Monoid a) => Gen a -> PropertyT m ()
+monoidRightIdentity gen = do
+  a <- forAll gen
+  a <> mempty === a
+
+monoidMappendCombine :: (Monad m, Show a, Eq a, Monoid a) => Gen a -> PropertyT m ()
+monoidMappendCombine gen = do
+  a <- forAll gen
+  b <- forAll gen
+  a <> b === mappend a b
